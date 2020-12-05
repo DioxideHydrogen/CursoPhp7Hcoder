24e92575645b6fe87a32150807e3827c1b352d87
Hugo Henrique <mxhugoxm@gmail.com>
2020-12-05 17:56:44 -0300

    More examples of DateTime Class

---
+++ Date/exemplo05.php
@@ -0,0 +1,7 @@
+<?php
+
+$dt = new DateTime();
+$periodo = new DateInterval("P15D");
+echo $dt->format("d/m/Y H:i:s") . "<br>";
+$dt->add($periodo);
+echo $dt->format("d/m/Y H:i:s");
--- readme.md
+++ readme.md
@@ -3,5 +3,5 @@
 
 [Link do Curso na Udemy](https://www.udemy.com/course/curso-php-7-online/)
 
-Curso iniciado em: 03/12/2020
+Curso iniciado em: 03/12/2020  
 Curso finalizado em:
